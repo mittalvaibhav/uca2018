b69e8e88d57bd26d860efe2d454ff42afe18b210
Vaibhav <mittal85.vaibhav@gmail.com>
2017-10-08 05:44:04 +0530

    Initial commit

---
+++ es6-project/js/main.js
@@ -0,0 +1,8 @@
+var batch = "UCA2018";
+let batch2 = "UCA2019";
+
+function getStudentDetails() {
+    document.getElementById("username").innerHTML = batch;
+}
+
+["a", "b"].map(name => `${name}!`);
