384e72c300bb32a0b902fdccb33db5f3b305fc46
Vaibhav <mittal85.vaibhav@gmail.com>
2018-02-27 22:50:24 +0530

    Added db config file

--- UniversityLibrary/models/book.js
+++ UniversityLibrary/models/book.js
@@ -22,6 +22,7 @@ router.get('', (req, res) => {
         if (err) {
             throw err
         } else {
+            console.log('Get books request process successfully');
             res.send(books);
         }
     })
